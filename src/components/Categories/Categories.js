ad2e376fffb3308eae33097eb02c698f466c5211
Soncl97 <soncl_0206@naver.com>
2020-04-20 17:00:59 +0900

    reinitializing

---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './Categories.scss';
+
+class Categories extends Component {
+  render() {
+    return(
+      <div class="categories">
+      </div>
+    )
+  }
+}
+
+export default withRouter(Categories);
