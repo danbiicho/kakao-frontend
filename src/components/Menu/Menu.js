c02e88ca36f0ffbc2b5c5b7c9a72c128d23448b8
danbiicho <danbiicho@gmail.com>
2020-04-22 09:56:03 +0900

    create main componenet

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,11 +3,15 @@ import { withRouter } from 'react-router-dom';
 import '../Menu/Menu.scss';
 
 class Menu extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {isClicked: ture};    
+  // }
   render() {
     return (
       <div class="menu">
         <ul className="list">
-          <li><button>홈</button></li>
+          <li><button onClick={this.handleClick}>홈</button></li>
           <li><button>신규</button></li>
           <li><button>인기</button></li>
           <li><button>세일</button></li>
