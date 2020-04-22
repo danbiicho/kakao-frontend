6a35aba8c2a1391e5624338ef1e2b80ceaf3b691
danbiicho <danbiicho@gmail.com>
2020-04-22 13:35:43 +0900

    충돌해결 중

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -11,7 +11,10 @@ class Menu extends Component {
     return (
       <div class="menu">
         <ul className="list">
-          <li><button onClick={this.handleClick}>홈</button></li>
+          <li>
+            <button onClick={this.handleClick}>홈</button>
+            <hr className="highlight" />
+          </li>
           <li><button>신규</button></li>
           <li><button>인기</button></li>
           <li><button>세일</button></li>
