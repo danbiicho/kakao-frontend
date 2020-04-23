3b73944e09e5c5db843ab118eeeec768d5675fb9
Soncl97 <soncl_0206@naver.com>
2020-04-23 21:20:56 +0900

    feature/toy-mini

---
+++ src/pages/Categories/Categories.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import CategoryToy from "./CategoryToy/CategoryToy";
+import "./Categories.scss";
+
+class Categories extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Categories">
+        <div className="categoriesHover">
+          <div
+            className="categoryWrapper"
+            onMouseLeave={this.props.onMouseLeave}
+          >
+            <ul className="categoryMenuUl">
+              <div class="categoryList1">
+                <li>전체</li>
+                <li>테마 기획전</li>
+                <li>토이</li>
+                <li>리빙</li>
+                <li>잡화 </li>
+                <li>문구</li>
+                <li>의류</li>
+                <li>파자마</li>
+                <li>여행/레져</li>
+                <li>생활테크</li>
+                <li>폰 액세서리</li>
+                <li>식품</li>
+              </div>
+              <CategoryToy />
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Categories);
---
+++ src/pages/Categories/CategoryToy/CategoryToy.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import "./CategoryToy.scss";
+
+class CategoryToy extends Component {
+  goToToyMiniDoll = () => {
+    this.props.history.push("/toyMiniDoll");
+  };
+
+  render() {
+    return (
+      <div className="CategoryToy" onMouseEnter={this.props.onMouseEnter}>
+        <ul className="categoryUl">
+          <li onClick={this.goToToyMiniDoll}>미니인형</li>
+          <li>중형인형</li>
+          <li>대형인형</li>
+          <li>키체인인형</li>
+          <li>피규어/브릭</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default withRouter(CategoryToy);
