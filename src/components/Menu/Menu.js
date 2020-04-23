44cf14b9e60dcc099587d45bff76735ee51f94b8
danbiicho <danbiicho@gmail.com>
2020-04-23 17:13:42 +0900

    메뉴 클릭 구현

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,22 +3,38 @@ import { withRouter } from 'react-router-dom';
 import '../Menu/Menu.scss';
 
 class Menu extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {isClicked: ture};    
-  // }
+  constructor() {
+    super();
+    this.state = {
+      activeTabId: 0
+    }
+  }
+
+  handleClick = (id) => {
+    this.setState({
+      activeTabId: Number(id),
+    });
+  }
+
   render() {
     return (
       <div class="Menu">
         <ul className="list">
-          <li className="Li1">
-            <button>홈</button>
-            <hr className="highlight" />
+          <li>
+            <button className={(this.state.activeTabId === 0) ? 'isStyleVisible' : 'listbutton'} onClick={() => { this.handleClick(0) }} >홈</button>
           </li>
-          <li className="2" ><button>신규</button></li>
-          <li className="3" ><button>인기</button></li>
-          <li ><button>세일</button></li>
-          <li><button>전체</button></li>
+          <li>
+            <button className={(this.state.activeTabId === 1) ? 'isStyleVisible' : 'listbutton'} onClick={() => { this.handleClick(1) }} >신규</button>
+          </li>
+          <li>
+            <button className={(this.state.activeTabId === 2) ? 'isStyleVisible' : 'listbutton'} onClick={() => { this.handleClick(2) }} >인기</button>
+          </li>
+          <li>
+            <button className={(this.state.activeTabId === 3) ? 'isStyleVisible' : 'listbutton'} onClick={() => { this.handleClick(3) }} >세일</button>
+          </li>
+          <li>
+            <button className={(this.state.activeTabId === 4) ? 'isStyleVisible' : 'listbutton'} onClick={() => { this.handleClick(4) }} >전체</button>
+          </li>
         </ul>
       </div>
     );
