97d9377407f5c7b8e6bf485c6469df52661b4403
Soncl97 <soncl_0206@naver.com>
2020-04-24 17:47:10 +0900

    feature/minitoy

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Pagination.scss";
+
+class Pagination extends Component {
+  render() {
+    return (
+      <div className="Pagination">
+        <span className="number"></span>
+      </div>
+    );
+  }
+}
+
+export default Pagination;
