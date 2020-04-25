d3667eacc81c07fac1715b1df581bf7f123baff6
Soncl97 <soncl_0206@naver.com>
2020-04-25 13:27:51 +0900

    신규페이지 수정

--- src/pages/New/NewArticle/NewArticle.js
+++ src/pages/New/NewArticle/NewArticle.js
@@ -2,7 +2,52 @@ import React, { Component } from "react";
 import "./NewArticle.scss";
 
 class NewArticle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      datas: [],
+    };
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:3000/data/itemLists.json")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState(
+          {
+            datas: res.item1,
+          },
+          () => console.log(this.state.datas)
+        );
+      });
+  };
+
   render() {
+    const { datas } = this.state;
+
+    const itemLists = datas.map((data) => {
+      return (
+        <li className="itemListWrap">
+          <div className="itemListWrap">
+            <img className="item" src={data.src} alt="" />
+              <div className="itemSubject">
+                <p className="topItemSubject">
+                {data.name}
+                </p>
+                <img className="buyImg"
+                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
+                    alt="" />
+              </div>
+              <div className="itemCost">
+                <span className="itemCostNum">{data.cost}</span>
+                <span className="won"> 원</span>
+              </div>
+              <div className="opacityWrap"></div>
+          </div>
+        </li>
+      );
+    });
+    
     return (
       <div className="NewArticle">
         <div className="articleWidth">
@@ -10,209 +55,7 @@ class NewArticle extends Component {
             <p className="topNewSubject">추천 신규 테마</p>
             <p className="bottomNewSubject">베이비 드리밍</p>
           </div>
-
-          <ul className="articleLists">
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-
-            <li className="itemListWrap">
-              <div className="itemListWrap">
-                <img
-                  className="item"
-                  src="https://t1.daumcdn.net/friends/prod/product/20200410105751049_8809681707756_AW_00.jpg?type=thumb&opt=R255x255@2xa"
-                  alt=""
-                />
-                <div className="itemSubject">
-                  <p className="topItemSubject">
-                    베이비드리밍 에어팟키링 리틀 라이언
-                  </p>
-                  <img
-                    className="buyImg"
-                    src="https://t1.kakaocdn.net/friends/new_store/2.0/common/basket-pink-3.png"
-                    alt=""
-                  />
-                </div>
-                <div className="itemCost">
-                  <span className="itemCostNum">12000</span>
-                  <span className="won"> 원</span>
-                </div>
-                <div className="opacityWrap"></div>
-              </div>
-            </li>
-          </ul>
-
+          <ul className="articleLists">{itemLists}</ul>
           <div className="addItemsWith">
             <div className="addItems">
               더 보기
@@ -222,6 +65,11 @@ class NewArticle extends Component {
               />
             </div>
           </div>
+          <div className="newSubject">
+            <p className="topNewSubject">오늘 업데이트 했어요</p>
+            <p className="bottomNewSubject">새로운 친구들</p>
+          </div>
+          <ul className="articleLists">{itemLists}</ul>
         </div>
       </div>
     );
