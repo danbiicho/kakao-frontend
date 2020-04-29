2151b87813cc875133953c903b5c6e9b45f269bc
Soncl97 <soncl_0206@naver.com>
2020-04-29 18:10:08 +0900

    상품 장바구니 버튼 선택기능

--- src/pages/BucketLists/BucketItemComponent/BucketItemComponent.js
+++ src/pages/BucketLists/BucketItemComponent/BucketItemComponent.js
@@ -2,25 +2,42 @@ import React, { Component } from "react";
 import "./BucketItemComponent.scss";
 
 class BucketItemComponent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      datas:[
+        {
+          "itemImg":"https://t1.daumcdn.net/friends/prod/product/20200423173253054_8809681709576_8809681709576_AW_00.jpg?type=thumb&opt=R255x255@2xa",
+          "name":"상품명",
+          "cost":20000,
+        }
+      ],
+      nums:[1],
+    }
+  }
+
   render() {
+    const { datas } = this.state;
     return(
-      <div className="BucketItemComponent">
+      datas.map((data) => (
+        <div className="BucketItemComponent">
         <li className="bucketItemLists">
           <div className="bucketLeft">
             <img className ="checkImg" src="https://t1.kakaocdn.net/friends/new_store/ico_checked.png" alt=""/>
             <div className="bucketItem">
-              <img className = "itemImg" src="https://t1.daumcdn.net/friends/prod/product/20200423173253054_8809681709576_8809681709576_AW_00.jpg?type=thumb&opt=R255x255@2xa" alt=""/>
-              <img className = "opacityWrap" alt=""/>
+              <img className = "itemImg" src={data.itemImg} alt=""/>
+              <div className = "opacityWrap" alt=""/>
             </div>
-            <span className="itemName">상품이름</span>
+          <span className="itemName">{data.name}</span>
           </div>
           <div className="bucketRight">
             <select className="selectNum"></select>
-            <span className="itemCost">상품금액</span>
-            <button className="deleteItem"><img src="https://t1.kakaocdn.net/friends/new_store/btn_remove.png" alt=""/></button>
+            <span className="itemCost">{data.cost}원</span>
+            <button className="deleteItem"><img className="deleteButton" src="https://t1.kakaocdn.net/friends/new_store/btn_remove.png" alt=""/></button>
           </div>
         </li>
-      </div>
+        </div>
+      ))
     )
   }
 }
