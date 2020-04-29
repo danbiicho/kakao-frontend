bb75fd41721d695a51993bbb3a6d6a0fee2602dd
danbi <danbiicho@gmail.com>
2020-04-29 16:33:31 +0900

    페이지네이션 구현 중

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,16 +4,62 @@ import Navbar from '../../components/Navbar/Navbar';
 import Menu from '../../components/Menu/Menu';
 import MainContents from '../../pages/Home/HomeComponents/MainContents/MainContents';
 import Footer from '../../components/Footer/Footer';
-// import CategoryHeader from '../../components/CategoryHeader/CategoryHeader';
-import Hot from '../Hot/Hot';
 import '../../styles/Reset.scss';
 import './Home.scss';
 
 
+let lastScrollY = 0;
+let ticking = false;
+
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topButton: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  nav = React.createRef();
+
+  handleScroll = () => {
+    let screenScroll = window.innerHeight;
+    lastScrollY = window.scrollY;
+    console.log("screenScroll", screenScroll);
+
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        this.nav.current.style.top = `${lastScrollY}px`;
+        ticking = false;
+        console.log(this.nav.current.style.top);
+
+        if (lastScrollY > 600) {
+          this.setState({
+            topButton: true,
+          })
+        } else {
+          this.setState({
+            topButton: false,
+          })
+        }
+      });
+
+      ticking = true;
+    }
+  };
+
   render() {
     return (
-      <div>
+      <div className="Home">
+        <nav ref={this.nav} />
+        <div className={(this.state.topButton) ? 'goTopButton' : 'button'}></div>
         <Navbar />
         <Menu />
         <MainContents />
--- src/pages/Hot/HotList.js
+++ src/pages/Hot/HotList.js
@@ -6,22 +6,23 @@ class HotList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // idx: [],
             cart: false,
+            offset: 18, // 처음에 화면에 보이는 이미지 개수
             datas: [],
+            perPage: 5,
+            currentPage: 1,
         };
     }
 
     handleCart = (i) => {
         this.setState({
-            // idx: [...this.state.idx].push(i),
             idx: i,
             cart: true,
         }, console.log(this.state.idx));
     }
 
     componentDidMount = () => {
-        fetch("http://localhost:3000/data/CartLists.json")
+        fetch("http://localhost:3000/data/HotLists.json")
             .then((res) => res.json())
             .then((res) => {
                 this.setState({
@@ -30,20 +31,31 @@ class HotList extends Component {
             });
     }
 
+    addData = () => {
+        console.log("200, ok");
+    }
+
     render() {
         const { datas } = this.state;
         return (
-
-            datas.map((data, i) => (
-                <div className="hotWrap">
-                    <img className="hotImg" src={data.src} alt="img" />
-                    <div className="cartWrap">
-                        <button className="hotButton" key={i} className={(this.state.idx === i) ? 'cartOn' : 'cartOff'} onClick={() => { this.handleCart(i) }}>
-                        </button>
+            <>
+                {datas.map((data, i) => (
+                    <div className="hotWrap">
+                        <img className="hotImg" src={data.src} alt="img" />
+                        <div className="cartWrap">
+                            <button className="hotButton" key={i} className={(this.state.idx === i) ? 'cartOn' : 'cartOff'} onClick={() => { this.handleCart(i) }}>
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))
+                ))}
+                <div className="contents">
+                    <div className="Pagination">
+                        <span className="number"></span>
+                        <button className="pageButton" onClick={this.addData}>더 보기</button>
+                    </div>
 
+                </div>
+            </>
         );
     }
 }
